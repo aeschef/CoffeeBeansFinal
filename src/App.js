ea3dc1535c8150704d89fbbd807ecdb1280c8a74
sruthikgunturu <sruthigunturu@utexas.edu>
2023-03-29 17:42:17 -0500

    nav bar issues

--- src/App.js
+++ src/App.js
@@ -10,9 +10,9 @@ import meal_plan from './meal_plan';
 import recipes from './recipes';
 import account from './account';
 
-{/*function App() {*/}
-export default class App extends React.Component{
-  render(){
+function App() {
+{/*export default class App extends React.Component{*/}
+  {/*render(){*/}
   return (
     <div className="App">
       <header className="App-header">
@@ -20,11 +20,16 @@ export default class App extends React.Component{
           <div>
             <NavbarElements />
             <Routes path="/" component={App}>
-              <Route path='grocery_list' render={() => <grocery_list></grocery_list>} />
+              {/*<Route path='grocery_list' render={() => <grocery_list></grocery_list>} />
               <Route exact path='inventory' render={() => <inventory></inventory>} />
               <Route exact path='meal_plan' render={() => <meal_plan></meal_plan>} />
               <Route exact path='recipes' render={() => <recipes></recipes>} />
-              <Route exact path='account' render={() => <account></account>} />
+              <Route exact path='account' render={() => <account></account>} /> */}
+              <Route path='grocery_list' component={grocery_list} />
+              <Route exact path='inventory' component={inventory} />
+              <Route exact path='meal_plan' component={meal_plan} />
+              <Route exact path='recipes' component={recipes} />
+              <Route exact path='account' component={account} />
             </Routes>
           </div>
         </Router>
@@ -32,8 +37,8 @@ export default class App extends React.Component{
       </header>
     </div>
   );
-  }
+  /*}*/
 
 }
 
-{/*export default App;*/}
+export default App;
--- src/grocery_list.js
+++ src/grocery_list.js
@@ -2,6 +2,20 @@ import React, { Component } from 'react'
 import logo from './logo.svg';
 import './App.css';
 
+const grocery_list = () => {
+    return(
+        <div>
+            <h1>Grocery List</h1>
+        </div>
+    );
+};
+
+export default grocery_list;
+
+{/*import React, { Component } from 'react'
+import logo from './logo.svg';
+import './App.css';
+
 export default class grocery_list extends Component {
     render() {
         return (
@@ -11,4 +25,4 @@ export default class grocery_list extends Component {
             </div>
         )
     }
-}
+}*/}
