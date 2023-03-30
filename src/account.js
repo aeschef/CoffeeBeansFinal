bb5c2f7155b449e5e91ac5a9f676d8d8489f9bee
Anna <anna.scheffrahn@gmail.com>
2023-03-30 00:22:03 -0500

    NavBar working dummy pages implemented

---
+++ src/account.js
@@ -0,0 +1,13 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import React, { useState, Component } from 'react';
+import Form from 'react-bootstrap/Form';
+import Modal from 'react-bootstrap/Modal';
+
+export default class AccountHome extends Component {
+    render() {
+        return <Button>Account</Button>;
+    }
+}
