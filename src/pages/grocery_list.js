f0e8be39cda1de676f000d0f36bea9e0bc106976
Anna <anna.scheffrahn@gmail.com>
2023-03-29 22:28:19 -0500

    Added category edit popup & padding @ bottom

--- src/pages/grocery_list.js
+++ src/pages/grocery_list.js
@@ -1,11 +1,51 @@
-import React, { Component } from 'react'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import { useState } from "react";
 import Button from 'react-bootstrap/Button';
+import React, { useState, Component } from 'react';
+import Form from 'react-bootstrap/Form';
+import Modal from 'react-bootstrap/Modal';
 
-
+/** popup for editing the Categorys */
+function CategorysPopup() {
+    const [show, setShow] = useState(false);
+  
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+  
+    return (
+      <>
+        <Button variant="primary" value="edit category" onClick={handleShow}>pencil icon</Button>
+  
+        <Modal show={show} onHide={handleClose} centered>
+          <Modal.Header closeButton>
+            <Modal.Title>Edit Category</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form>
+              <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                <Form.Label>Category Title</Form.Label>
+                <Form.Control
+                  size="sm"
+                  type="text"
+                  placeholder=""
+                  autoFocus
+                />
+              </Form.Group>
+            </Form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Delete Category
+            </Button>
+            <Button variant="primary" onClick={handleClose}>
+              Save Changes
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </>
+    );
+  }
 
 
 /**
@@ -39,7 +79,7 @@ function ListCategory (){
                     <span>category name</span>
                 </Col>
                 <Col>
-                    <button>pencil icon</button>
+                    <CategorysPopup></CategorysPopup>                       
                 </Col>
             
                 <ItemList></ItemList>
@@ -51,42 +91,56 @@ function ListCategory (){
     );
 }
 
-
+const AddItem = () => {
+    return (
+        <>
+            <Button className="add-btn">Add</Button>        
+        </>
+    );
+};
 export default class GroceryListHome extends Component {
     
     render() {
         return (
-            <Container fluid="md">
-                <Row>
-                    <Col xs={{span:6, offset:3}}>
-                        <h1>Grocery List</h1>
-                    </Col>
-                    <Col xs={{span:2}}>
-                        <Button>filter</Button>
-                    </Col>
-                </Row>
+                <Container fluid="md">
+                    <Row>
+                        <Col xs={{span:6, offset:3}}>
+                            <h1>Grocery List</h1>
+                        </Col>
+                        <Col xs={{span:2}}>
+                            <Button>filter</Button>
+                        </Col>
+                    </Row>
 
-                <Row>
-                    <Col>
-                        <div className="d-grid gap-2">
-                            <Button>personal</Button>
-                        </div>
-                    </Col>
-                    <Col>
-                        <div className="d-grid gap-2">
-                            <Button>shared</Button>
-                        </div>
-                        
-                    </Col>
-                </Row>
-                <ListCategory></ListCategory>
-                <ListCategory></ListCategory>
-                <ListCategory></ListCategory>
-                <ListCategory></ListCategory>
-                
+                    <Row>
+                        <Col>
+                            <div className="d-grid gap-2">
+                                <Button>personal</Button>
+                            </div>
+                        </Col>
+                        <Col>
+                            <div className="d-grid gap-2">
+                                <Button>shared</Button>
+                            </div>
+                            
+                        </Col>
+                    </Row>
+                    <ListCategory></ListCategory>
+                    <ListCategory></ListCategory>
+                    <ListCategory></ListCategory>
+                    <ListCategory></ListCategory>
+                    <AddItem></AddItem>
+
+                </Container>
 
 
-            </Container>
+// <Row>
+// <Col className="mb-sm-10" xs={{offset:8}}>
+//     <div className="position-fixed">
+//         <Button>ADD</Button>
+//     </div>
+// </Col>
+// </Row>
         )
     }
 }
