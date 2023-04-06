ec07ab901bf07eaeeca1b2971a2a4d00beff8dfe
Anna <anna.scheffrahn@gmail.com>
2023-04-05 19:17:12 -0500

    commented out edit icon

--- src/modals/EditGLICategories.jsx
+++ src/modals/EditGLICategories.jsx
@@ -18,14 +18,15 @@ const CategorysPopup = () => {
     const handleShow = () => setShow(true);
 //            <Button variant="primary" value="edit category" onClick={handleShow}>pencil icon</Button>
 
-            
-    return (
-        <>
-            <Col>
+{/* <Col>
                 <a href="#" onClick={handleShow} className="pe-auto left-spacing">
                     <img src={PencilIcon} alt="Edit Pencil Icon" className="pencil-icon"/>
                 </a>          
             </Col> 
+             */}
+    return (
+        <>
+            
             <Modal show={show} onHide={handleClose} centered>
                 <Modal.Header closeButton>
                     <Modal.Title>Edit Category</Modal.Title>
