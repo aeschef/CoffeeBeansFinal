ee48fdede8417262d1a9852ef3b6a9c38edcad10
Anna <anna.scheffrahn@gmail.com>
2023-04-07 23:25:18 -0500

    Added necessary database information

--- src/App.js
+++ src/App.js
@@ -5,6 +5,32 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import NavbarElements from './NavbarElements';
 import LoginHome from './login';
 
+
+// Import the functions you need from the SDKs you need
+//Authorization imports
+import { initializeApp } from "firebase/app";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut } from "firebase/auth";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB45KzUwC2AeFXHHcaAD6A0Mu1Mg77s-2U",
+  authDomain: "coffee-beans-final.firebaseapp.com",
+  databaseURL: "https://coffee-beans-final-default-rtdb.firebaseio.com",
+  projectId: "coffee-beans-final",
+  storageBucket: "coffee-beans-final.appspot.com",
+  messagingSenderId: "260660376463",
+  appId: "1:260660376463:web:a3b701b0b7512c040791fa"
+};
+
+const databaseURL = "https://coffee-beans-final-default-rtdb.firebaseio.com";
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 function App() {
 
   // determines if user is logged in or not.
