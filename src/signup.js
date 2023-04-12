33f6100de889718afd1a5585c384573289deefc4
Anna <anna.scheffrahn@gmail.com>
2023-04-12 00:28:12 -0500

    create grou pworks! joining group does not yet!

--- src/signup.js
+++ src/signup.js
@@ -3,6 +3,7 @@ import './App.css';
 import './css/meal_plan.css';
 import Form from 'react-bootstrap/Form'
 import { useEffect, useState } from 'react';
+import Modal from "react-bootstrap/Modal";
 import { getDatabase, ref, set } from "firebase/database";
 
 
@@ -12,17 +13,49 @@ const SignupHome = ({createUser, login, setLogin, auth}) => {
   const [password, setPassword] = useState("");
   const [user, setUser] = useState("");
   const [confirmUser, setConfirmUser] = useState("");
+  const [created, setAccountCreated] = useState(false);
+  const [groupName, setGroupName] = useState("");
+  const [accessCode, setAccess] = useState("");
+
 
   // Keeps track of when user entered invalid login information
   const [error, setError] = useState(false)
 
+
+  const handleGroupClose = () => {
+
+  };
+
+  //Allows this user to join groups! 
+  const joinGroup =() => {
+
+    //
+  };
+
+  // Creates a new Roommate Group in the database
+  const createNewGroup = () => {
+    //TODO make it unique
+    let uniqueGroupID = Math.random().toString(6).slice(2, 7); 
+    
+    const db = getDatabase();
+    set(ref(db, 'groups/' + uniqueGroupID), {
+      group_name: {groupName},
+      members: {user},
+      // only needs shared.. other in the account
+      grocery_list: {0:""},
+      inventory:{0:""}
+    });
+    setLogin(true);
+  };
+  
+  //interacts with the database to authorize the current user with this email 
+  // and password 
   const handleCreateAcct = () => {
     if(password && user) {
+      // values in both boxes so create account
       createUser(auth, user, password)
         .then((userCredential) => {
-          //Loggedin
-          setLogin(true);
-
+          //logged in
           console.log("current user is " + auth.currentUser);
           console.log(auth.currentUser.uid);
         })
@@ -32,6 +65,7 @@ const SignupHome = ({createUser, login, setLogin, auth}) => {
           console.log(errorMessage);
         });
 
+      // populate this user's part of the database with structure...
       const db = getDatabase();
       set(ref(db, 'users/' + auth.currentUser.uid), {
         account: {
@@ -50,6 +84,8 @@ const SignupHome = ({createUser, login, setLogin, auth}) => {
         },
         recipes: {0:""}  
       });
+
+      setAccountCreated(true);
 
 
     } else{
@@ -85,6 +121,33 @@ const SignupHome = ({createUser, login, setLogin, auth}) => {
               Submit
             </Button>
           </Form>
+
+
+          <Modal show={created} onHide={handleGroupClose} centered>
+            <Modal.Header closeButton>
+              <Modal.Title>Group</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <Form>
+                <Form.Group className="mb-3">
+                  <Form.Label >Create New Group</Form.Label>
+                  <Form.Control type="text" placeholder="Group Name" value={groupName} onChange={(e)=>setGroupName(e.target.value)}/>
+                </Form.Group>
+                <Button variant="primary" onClick={createNewGroup}>
+                  Create 
+                </Button>
+
+                <Form.Group className="mb-3">
+                  <Form.Label >Join Group</Form.Label>
+                  <Form.Control type="text" placeholder="Access Code" value={accessCode} onChange={(e)=>setAccess(e.target.value)}/>
+                </Form.Group>
+                <Button variant="primary" onClick={joinGroup} >
+                  Join
+                </Button>
+
+              </Form>
+            </Modal.Body>
+          </Modal>
         </div>
       <div className="d-flex justify-content-center align-items-center"><h5>Don't have an account? <a href="./meal_plan">Sign up</a></h5></div>
     </div>
