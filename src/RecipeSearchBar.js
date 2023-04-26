8c1b17668507b977e95b98c31c5451a1c33f40fa
Megan Sickler <megansickler@utexas.edu>
2023-04-26 13:00:45 -0500

    added icon to search bar

--- src/RecipeSearchBar.js
+++ src/RecipeSearchBar.js
@@ -1,6 +1,8 @@
 // credit: https://plainenglish.io/blog/how-to-implement-a-search-bar-in-react-js
 
 import React, {useState} from 'react'
+import SearchIcon from './svg/search.svg'
+import './css/search.css'
 
 export default function SearchBar(props) {
     
@@ -9,11 +11,14 @@ export default function SearchBar(props) {
     }
 
     return (
-        <input
-            type="text"
-            placeholder={props.placeholder}
-            onChange={handleChange}
-            value={props.searchInput}>
-        </input>
+        <div className='search-bar'>
+            <img src={SearchIcon}></img>
+            <input
+                type="text"
+                placeholder={props.placeholder}
+                onChange={handleChange}
+                value={props.searchInput}>
+            </input>
+        </div>
     )
 }
